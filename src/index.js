5ec36572c1cf2bb1f61bfdad67d1dbb4a2ece89e
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2026-08-30 12:48:58 +0000

    utils created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import 'dotenv/config' ;
+import "dotenv/config";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed!!! :", err);
+  });
